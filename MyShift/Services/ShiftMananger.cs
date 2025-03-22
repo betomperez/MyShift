b784df55576b7aa582e81b9d8817fe110354dd0b
betomperez <betomperez@gmail.com>
2025-03-22 10:17:43 -0300

    Refactored ShiftManager class to use ShiftNames and i18n.

--- MyShift/Services/ShiftMananger.cs
+++ MyShift/Services/ShiftMananger.cs
@@ -1,14 +1,87 @@
-﻿namespace MyShift.Services;
+﻿using MyShift.Models;
+using MyShift.Resources.Languages;
+
+namespace MyShift.Services;
 
 class ShiftMananger
 {
-    private Dictionary<string, List<string>> _shifts = new Dictionary<string, List<string>>
+    private Dictionary<ShiftNames, List<string>> _shifts = new Dictionary<ShiftNames, List<string>>
     {
-        {"morning", new List<string>    { "A", "A", "A", "A", "B", "B", "B", "B", "C", "C" } },
-        {"afternoon", new List<string>  { "C", "C", "D", "D", "D", "D", "E", "E", "E", "E" } },
-        {"night", new List<string>      { "E", "E", "C", "C", "A", "A", "D", "D", "B", "B" } },
-        {"dayoffA", new List<string>    { "B", "D", "E", "B", "C", "E", "A", "C", "D", "A" } },
-        {"dayoffB", new List<string>    { "D", "B", "B", "E", "E", "C", "C", "A", "A", "D" } },
+        {
+            ShiftNames.Morning, new List<string>
+            {
+                $"A - {AppStrings.FirstMorning}", 
+                $"A - {AppStrings.SecondMorning}",
+                $"A - {AppStrings.ThirdMorning}",
+                $"A - {AppStrings.FourthMorning}",
+                $"B - {AppStrings.FirstMorning}",
+                $"B - {AppStrings.SecondMorning}",
+                $"B - {AppStrings.ThirdMorning}",
+                $"B - {AppStrings.FourthMorning}",
+                $"C - {AppStrings.FirstMorning}",
+                $"C - {AppStrings.SecondMorning}" 
+            } 
+        },
+        {
+            ShiftNames.Afternoon, new List<string>  
+            {
+                $"C - {AppStrings.FirstAfternoon}",
+                $"C - {AppStrings.SecondAfternoon}",
+                $"D - {AppStrings.FirstAfternoon}",
+                $"D - {AppStrings.SecondAfternoon}",
+                $"D - {AppStrings.ThirdAfternoon}",
+                $"D - {AppStrings.FourthAfternoon}",
+                $"E - {AppStrings.FirstAfternoon}",
+                $"E - {AppStrings.SecondAfternoon}",
+                $"E - {AppStrings.ThirdAfternoon}",
+                $"E - {AppStrings.FourthAfternoon}"
+            } 
+        },
+        {
+            ShiftNames.Night, new List<string>
+            {
+                $"E - {AppStrings.FirstNight}",
+                $"E - {AppStrings.SecondNight}",
+                $"C - {AppStrings.FirstNight}",
+                $"C - {AppStrings.SecondNight}",
+                $"A - {AppStrings.FirstNight}",
+                $"A - {AppStrings.SecondNight}",
+                $"D - {AppStrings.FirstNight}",
+                $"D - {AppStrings.SecondNight}",
+                $"B - {AppStrings.FirstNight}",
+                $"B - {AppStrings.SecondNight}"
+            } 
+        },
+        {
+            ShiftNames.DayOff, new List<string>
+            { 
+                $"B - {AppStrings.FirstDayOff}",
+                $"D - {AppStrings.FourthDayOff}",
+                $"E - {AppStrings.FirstDayOff}",
+                $"B - {AppStrings.FourthDayOff}",
+                $"C - {AppStrings.FirstDayOff}",
+                $"E - {AppStrings.FourthDayOff}",
+                $"A - {AppStrings.FirstDayOff}",
+                $"C - {AppStrings.FourthDayOff}",
+                $"D - {AppStrings.FirstDayOff}",
+                $"A - {AppStrings.FourthDayOff}"
+            }
+        },
+        {
+            ShiftNames.DayOffOT, new List<string> 
+            { 
+                $"D - {AppStrings.ThirdDayOff}", 
+                $"B - {AppStrings.SecondDayOff}", 
+                $"B - {AppStrings.ThirdDayOff}", 
+                $"E - {AppStrings.SecondDayOff}", 
+                $"E - {AppStrings.ThirdDayOff}", 
+                $"C - {AppStrings.SecondDayOff}", 
+                $"C - {AppStrings.ThirdDayOff}", 
+                $"A - {AppStrings.SecondDayOff}", 
+                $"A - {AppStrings.ThirdDayOff}", 
+                $"D - {AppStrings.SecondDayOff}"
+            }
+        },
     };
 
     public string? MorningShift { get; set; }
@@ -20,10 +93,11 @@ class ShiftMananger
     public void RefreshShifts(DateOnly date, int offset)
     {
         int index = (date.DayNumber + offset) % 10;
-        MorningShift = _shifts["morning"][index];
-        AfternoonShift = _shifts["afternoon"][index];
-        NightShift = _shifts["night"][index];
-        DayOffA = _shifts["dayoffA"][index];
-        DayOffB = _shifts["dayoffB"][index];
+
+        MorningShift = _shifts[ShiftNames.Morning][index];
+        AfternoonShift = _shifts[ShiftNames.Afternoon][index];
+        NightShift = _shifts[ShiftNames.Night][index];
+        DayOffA = _shifts[ShiftNames.DayOff][index];
+        DayOffB = _shifts[ShiftNames.DayOffOT][index];
     }
 }
