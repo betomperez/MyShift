91b48ab488fceac5c14cedaff6cdb23d69ab863c
betomperez <betomperez@gmail.com>
2025-03-23 22:22:24 -0300

    Implemented NotesManager class to deal with notes using File System.

--- MyShift/Pages/TeamViewPage.xaml.cs
+++ MyShift/Pages/TeamViewPage.xaml.cs
@@ -6,13 +6,15 @@ public partial class TeamViewPage : ContentPage
 {
     private ShiftMananger _shiftManager;
     private int offset = 4;
+
+    private NotesMananger _notesManager;
     public TeamViewPage()
 	{
 		InitializeComponent();
         _shiftManager = new ShiftMananger();
+        _notesManager = new NotesMananger();
         BindingContext = _shiftManager;
         _shiftManager.RefreshShifts(DateOnly.FromDateTime(DateView.Date), offset);
-
     }
 
     private void OnYesterdayButtonClicked(object sender, EventArgs e)
@@ -36,5 +38,26 @@ public partial class TeamViewPage : ContentPage
     private void OnDateViewDateSelected(object sender, DateChangedEventArgs e)
     {
         _shiftManager.RefreshShifts(DateOnly.FromDateTime(DateView.Date), offset);
+        DateTime selectedDateTime = e.NewDate;
+        if (!_notesManager.upToDateDictionary) 
+        {
+            _notesManager.LoadNotes();
+            _notesManager.upToDateDictionary = true;
+        }
+        if (_notesManager.notes.ContainsKey(selectedDateTime))
+        {
+            EditorNotes.Text = _notesManager.notes[selectedDateTime];
+        }
+        else
+        {
+            EditorNotes.Text = "";
+        }
+    }
+
+    private void OnEditorNotesTextChanged(object sender, TextChangedEventArgs e)
+    {
+        DateTime selectedDate = DateView.Date;
+        _notesManager.notes[selectedDate] = EditorNotes.Text;
+        _notesManager.SaveNotes();
     }
 }
---
+++ MyShift/Services/NotesMananger.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Enumeration;
+using System.Text.Json;
+
+namespace MyShift.Services;
+
+class NotesMananger
+{
+    public Dictionary<DateTime, String> notes = new Dictionary<DateTime, string>();
+
+    public bool upToDateDictionary = false;
+
+    private string _fileName = "mynotes.json";
+
+    public void SaveNotes()
+    {
+        string path = Path.Combine(FileSystem.AppDataDirectory, _fileName);
+        string json = JsonSerializer.Serialize(notes);
+        File.WriteAllText(path, json);
+    }
+
+    public void LoadNotes()
+    {
+        string path = Path.Combine(FileSystem.AppDataDirectory, _fileName);
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            notes = JsonSerializer.Deserialize<Dictionary<DateTime, string>>(json)!;
+            upToDateDictionary = false;
+        }
+    }
+}
